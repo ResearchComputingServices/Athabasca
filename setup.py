d5b2e0b021b32a89b4fdc220c45dfde7908a79a2
Nicholi Shiell <nicholishiell@gmail.com>
2024-05-01 15:00:05 -0400

    folder structure changed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="athabasca",
+    version="0.0.1",
+    description="Sematic sentence classifier using logistics regression and LLM generated sentence embeddings",
+    install_requires=[],
+    packages=["athabasca"],
+)
---
+++ src/SentenceClassifier/__init__.py
@@ -0,0 +1,2 @@
+from . import DataSet
+from . import Classifier
